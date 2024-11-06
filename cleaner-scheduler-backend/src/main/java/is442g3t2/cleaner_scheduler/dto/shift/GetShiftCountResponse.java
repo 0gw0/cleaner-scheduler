920b3b1b84a2ecbbec32d4e05e03dc0388dfc789
fraser chua <fraser.chua.2022@scis.smu.edu.sg>
2024-11-07 02:36:02 +0800

    Fix: Update get all shifts by workerId endpoint

--- cleaner-scheduler-backend/src/main/java/is442g3t2/cleaner_scheduler/dto/shift/GetShiftCountResponse.java
+++ cleaner-scheduler-backend/src/main/java/is442g3t2/cleaner_scheduler/dto/shift/GetShiftCountResponse.java
@@ -1,9 +1,11 @@
 package is442g3t2.cleaner_scheduler.dto.shift;
-
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.YearMonth;
+
+import is442g3t2.cleaner_scheduler.models.shift.Shift;
 
 @Setter
 @Getter
@@ -11,15 +13,18 @@ public class GetShiftCountResponse {
     private Long workerId;
     private int shiftCount;
     private YearMonth yearMonth;
+    private List<Shift> shifts;
 
-    public GetShiftCountResponse(Long workerId, int shiftCount) {
+    public GetShiftCountResponse(Long workerId, int shiftCount, List<Shift> shifts) {
         this.workerId = workerId;
         this.shiftCount = shiftCount;
+        this.shifts = shifts;
     }
 
-    public GetShiftCountResponse(Long workerId, int shiftCount, YearMonth yearMonth) {
+    public GetShiftCountResponse(Long workerId, int shiftCount, YearMonth yearMonth, List<Shift> shifts) {
         this.workerId = workerId;
         this.shiftCount = shiftCount;
         this.yearMonth = yearMonth;
+        this.shifts = shifts;
     }
 }
