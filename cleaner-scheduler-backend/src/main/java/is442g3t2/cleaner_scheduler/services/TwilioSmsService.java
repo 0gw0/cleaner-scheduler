e713518d2b15bd5e3c19df96733a2e19b98fae6c
lynnkwl <143063371+lynnkwl@users.noreply.github.com>
2024-11-08 21:36:59 +0800

    feat: 10 min reminders, uncommented!

--- cleaner-scheduler-backend/src/main/java/is442g3t2/cleaner_scheduler/services/TwilioSmsService.java
+++ cleaner-scheduler-backend/src/main/java/is442g3t2/cleaner_scheduler/services/TwilioSmsService.java
@@ -1,40 +1,40 @@
-// package is442g3t2.cleaner_scheduler.services;
+package is442g3t2.cleaner_scheduler.services;
 
-// import com.twilio.Twilio;
-// import com.twilio.rest.api.v2010.account.Message;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.stereotype.Service;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 
-// @Service
-// public class TwilioSmsService {
+@Service
+public class TwilioSmsService {
 
-//     @Value("${twilio.account.sid}")
-//     private String accountSid;
+    @Value("${twilio.account.sid}")
+    private String accountSid;
 
-//     @Value("${twilio.auth.token}")
-//     private String authToken;
+    @Value("${twilio.auth.token}")
+    private String authToken;
 
-//     @Value("${twilio.phone.number}")
-//     private String fromPhoneNumber;
+    @Value("${twilio.phone.number}")
+    private String fromPhoneNumber;
 
-//     public TwilioSmsService(
-//         @Value("${twilio.account.sid}") String accountSid,
-//         @Value("${twilio.auth.token}") String authToken,
-//         @Value("${twilio.phone.number}") String fromPhoneNumber
-//     ) {
-//         this.accountSid = accountSid;
-//         this.authToken = authToken;
-//         this.fromPhoneNumber = fromPhoneNumber;
+    public TwilioSmsService(
+        @Value("${twilio.account.sid}") String accountSid,
+        @Value("${twilio.auth.token}") String authToken,
+        @Value("${twilio.phone.number}") String fromPhoneNumber
+    ) {
+        this.accountSid = accountSid;
+        this.authToken = authToken;
+        this.fromPhoneNumber = fromPhoneNumber;
 
-//         Twilio.init(accountSid, authToken);
-//     }
+        Twilio.init(accountSid, authToken);
+    }
 
-//     public void sendSms(String toPhoneNumber, String messageContent) {
-//         Message.creator(
-//             new com.twilio.type.PhoneNumber(toPhoneNumber),
-//             new com.twilio.type.PhoneNumber(fromPhoneNumber),
-//             messageContent
-//         ).create();
-//     }
-// }
+    public void sendSms(String toPhoneNumber, String messageContent) {
+        Message.creator(
+            new com.twilio.type.PhoneNumber(toPhoneNumber),
+            new com.twilio.type.PhoneNumber(fromPhoneNumber),
+            messageContent
+        ).create();
+    }
+}
