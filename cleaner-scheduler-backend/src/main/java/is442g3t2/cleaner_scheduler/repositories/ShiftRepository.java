ed0f070f35801c02a1046b5485c671eacc009075
fraser chua <fraser.chua.2022@scis.smu.edu.sg>
2024-11-09 16:24:13 +0800

    Feat: Scheduler function to update shift status if no acknowledgement from worker
    Scheduler function to update shift status if no arrival photo uploaded by worker 15 minutes after shift start time

--- cleaner-scheduler-backend/src/main/java/is442g3t2/cleaner_scheduler/repositories/ShiftRepository.java
+++ cleaner-scheduler-backend/src/main/java/is442g3t2/cleaner_scheduler/repositories/ShiftRepository.java
@@ -1,9 +1,23 @@
 package is442g3t2.cleaner_scheduler.repositories;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 import is442g3t2.cleaner_scheduler.models.shift.Shift;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
+
+        // List<Shift> findByShiftStatusAndStartTimeBefore(ShiftStatus status, LocalDateTime dateTime);
+        @Query("SELECT s FROM Shift s WHERE s.date = :currentDate AND s.startTime <= :checkTime AND s.status = 'UPCOMING'")
+        List<Shift> findByShiftStatusAndStartTimeBefore(
+            @Param("currentDate") LocalDate currentDate,
+            @Param("checkTime") LocalTime checkTime
+        );
 }
